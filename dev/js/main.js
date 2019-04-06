e7979da5c262c1957a04bd0dcfdf7a8efc3346de
Yuri Adonev <zyztem@mail.ru>
2019-04-06 18:24:44 +0700

    Разметка главной

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
-    svg4everybody({});
+  svg4everybody({});
+  
+	$('.menu-toggle').click(function(){
+		$('.menu-toggle').toggleClass('menu-toggle--close');
+		$('.main-nav__wrap').toggleClass('active');
+		$('.side-nav').toggleClass('mobile');
+		$('body').toggleClass('no-scroll');
+	});
+	const mobileMenu = document.querySelector(".main-nav__wrap");
+	const mobileMenuBtn = document.querySelector(".menu-toggle");
+	
+	mobileMenuBtn.addEventListener('click', function(e) {
+		mobileMenu
+	});
+	
 });
